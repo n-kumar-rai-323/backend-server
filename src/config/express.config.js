fa28ae9ece7ea0fc51ed4282a1d9d64dc96b6f07
Nishan Rai <infonkumarrai323@gmail.com>
2026-08-12 22:20:03 +0545

    post request

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -3,5 +3,18 @@ const userRoutes = require("./router.config")
 
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+
+// app.use((req,res,next)=>{
+//     console.log("I am always executed")
+//     next() 
+// })
+
+// app.use((req,res,next)=>{
+//     console.log("I am always executed")
+//     next()
+// })
+
 app.use("/api/users", userRoutes)
 module.exports = app
--- src/config/router.config.js
+++ src/config/router.config.js
@@ -3,95 +3,106 @@ const router = require("express").Router()
 
 
 
-router.get("/", (req, res, next) => {
+// router.get("/", (req, res, next) => {
 
-    const data = {
-        data: [
-            {
-                id: 1,
-                name: "Nishan Rai",
-                email: "nishan@gmail.com"
-            },
-            {
-                id: 2,
-                name: "Sita Sharma",
-                email: "sita.sharma@gmail.com"
-            },
-            {
-                id: 3,
-                name: "Ramesh Thapa",
-                email: "ramesh.thapa@gmail.com"
-            },
-            {
-                id: 4,
-                name: "Anita Gurung",
-                email: "anita.gurung@gmail.com"
-            },
-            {
-                id: 5,
-                name: "Bikash Shrestha",
-                email: "bikash.shrestha@gmail.com"
-            },
-            {
-                id: 6,
-                name: "Sunita Basnet",
-                email: "sunita.basnet@gmail.com"
-            },
-            {
-                id: 7,
-                name: "Prakash Adhikari",
-                email: "prakash.adhikari@gmail.com"
-            },
-            {
-                id: 8,
-                name: "Kabita Karki",
-                email: "kabita.karki@gmail.com"
-            },
-            {
-                id: 9,
-                name: "Deepak Magar",
-                email: "deepak.magar@gmail.com"
-            },
-            {
-                id: 10,
-                name: "Rita Poudel",
-                email: "rita.poudel@gmail.com"
-            }
-        ],
-        message: "Users retrieved successfully",
-        status: "Success"
+//     const data = {
+//         data: [
+//             {
+//                 id: 1,
+//                 name: "Nishan Rai",
+//                 email: "nishan@gmail.com"
+//             },
+//             {
+//                 id: 2,
+//                 name: "Sita Sharma",
+//                 email: "sita.sharma@gmail.com"
+//             },
+//             {
+//                 id: 3,
+//                 name: "Ramesh Thapa",
+//                 email: "ramesh.thapa@gmail.com"
+//             },
+//             {
+//                 id: 4,
+//                 name: "Anita Gurung",
+//                 email: "anita.gurung@gmail.com"
+//             },
+//             {
+//                 id: 5,
+//                 name: "Bikash Shrestha",
+//                 email: "bikash.shrestha@gmail.com"
+//             },
+//             {
+//                 id: 6,
+//                 name: "Sunita Basnet",
+//                 email: "sunita.basnet@gmail.com"
+//             },
+//             {
+//                 id: 7,
+//                 name: "Prakash Adhikari",
+//                 email: "prakash.adhikari@gmail.com"
+//             },
+//             {
+//                 id: 8,
+//                 name: "Kabita Karki",
+//                 email: "kabita.karki@gmail.com"
+//             },
+//             {
+//                 id: 9,
+//                 name: "Deepak Magar",
+//                 email: "deepak.magar@gmail.com"
+//             },
+//             {
+//                 id: 10,
+//                 name: "Rita Poudel",
+//                 email: "rita.poudel@gmail.com"
+//             }
+//         ],
+//         message: "Users retrieved successfully",
+//         status: "Success"
 
-    };
+//     };
 
-    res.json(
+//     res.json(
+//         {
+//             data: data,
+//             message: "Health OK",
+//             status: "Success",
+//             options: null
+//         }
+//     )
+// })
+// const firstLoggeIn=(req,res,next)=>{
+//     console.log('Time:', Date.now());
+//     next()
+// }
+
+// const isRoleCheck=(req,res,next)=>{
+//     console.log("You are Admin")
+//     next()
+// }
+
+router.post("/register",(req,res,next)=>{
+    let data= req.body
+    res.status(200).json(
         {
-            data: data,
-            message: "Health OK",
-            status: "Success",
-            options: null
+            data:data,
+            message:"Register Successfully",
+            status:"Success",
+            options:null
         }
-    )
-})
-
+    )}
+)
 router.get("/:id", (req, res, next) => {
-    const response = {
-        data:
-        {
-            id: req.params.id,
-            name: "nishan Rai",
-            email: "nisharai@gmal.com"
-        },
 
-        message: "User retrived successfully",
-        status: "Success"
-    }
 
-    res.json(response)
+    let params = req.params
+    let query= req.query
+    let headers = req.headers
+    let body=req.body
+    res.json({
+        data:{params,query,headers, body}
+    })
 })
 
-
-
-router.post("/", (req, res, next) => {
-
-})
 module.exports = router
